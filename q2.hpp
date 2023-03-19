9d54c9e0d2b1b93d565f967aa5f0ae9a6ae28cca
LEEEJMONA <123528104+LEEEJMONA@users.noreply.github.com>
2023-03-19 16:20:38 -0700

    q2

--- q2.hpp
+++ q2.hpp
@@ -2,6 +2,41 @@
 #include <iomanip>
 using namespace std;
 
-// ******************************
-// Implement all your functions here
-// ******************************
+// int main()
+// {
+// 	int n1, n2, n3;
+// 	int min;
+// 	getInput(n1, n2, n3);
+// 	min = findMin(n1, n2, n3);
+// 	printResult(n1, n2, n3, min);
+// }
+
+void getInput(int &n1, int &n2, int &n3);
+int  findMin(int n1, int n2, int n3); 
+void printResult(int n1, int n2, int n3, int min);
+
+
+void getInput(int &n1, int &n2, int &n3)
+{
+    cin >> n1 >> n2 >> n3;
+}
+int  findMin(int n1, int n2, int n3)
+{
+    int min;
+
+    if (n1 < n2 && n1 < n3) 
+        min = n1;
+
+    else if (n2 < n1 && n2 < n3)
+        min = n2;
+        
+    else
+        min = n3;
+    
+    return min;
+}
+
+void printResult(int n1, int n2, int n3, int min)
+{
+    cout << n1 << " " << n2 << " " << n3 <<" "<< min;
+}
